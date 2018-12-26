c7168860c685c703b1876da4a1e72e1f351da37d
linyzh <35281069+linyzh@users.noreply.github.com>
2018-12-26 14:44:21 +0800

    Add files via upload

--- src/main/java/com/benliu/vo/AnalysisParamVo.java
+++ src/main/java/com/benliu/vo/AnalysisParamVo.java
@@ -1,1 +1,28 @@
+package com.benliu.vo;
 
+import lombok.Data;
+
+@Data
+public class AnalysisParamVo {
+
+	/**
+	 * 通信机ID
+	 */
+	private Integer machineId;
+	
+	/**
+	 * 起始时间
+	 */
+	private String dateBegin;
+	
+	/**
+	 * 结束时间
+	 */
+	private String dateEnd;
+	
+	/**
+	 * 数据类型
+	 */
+	private Integer dataType;
+	
+}
